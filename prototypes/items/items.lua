48b897b802f2329cfe68a12a6cfe977ebb22e235
szentigrad3 <nofukn@gmail.com>
2020-09-15 23:07:43 +1000

    fix catch only biters, update items

--- prototypes/items/items.lua
+++ prototypes/items/items.lua
@@ -5,7 +5,7 @@ data:extend(
     name = "small-biter-power",
     icons =
 	{
-        {icon = "__alien_power__/graphics/icons/biter.png"}
+        {icon = "__alien_power__/graphics/icons/small-biter.png"}
     },
     icon_size = 64,
     flags = {},
@@ -21,7 +21,97 @@ data:extend(
     name = "small-used-biter",
     icons =
 	{
-        {icon = "__alien_power__/graphics/icons/biter.png"},
+        {icon = "__alien_power__/graphics/icons/small-biter.png"},
+        {icon = "__alien_power__/graphics/icons/tired.png"},
+    },
+	icon_size = 64,
+    flags = {},
+    subgroup = "alien-power",
+    order = "x",
+    stack_size = 1
+	},
+	{
+    type = "item",
+    name = "medium-biter-power",
+    icons =
+	{
+        {icon = "__alien_power__/graphics/icons/medium-biter.png"}
+    },
+    icon_size = 64,
+    flags = {},
+    subgroup = "alien-power",
+    order = "a",
+    stack_size = 300,
+    fuel_category = 'alien',
+    fuel_value = '250MJ',
+    burnt_result = 'medium-used-biter'
+	},
+	{
+    type = "item",
+    name = "medium-used-biter",
+    icons =
+	{
+        {icon = "__alien_power__/graphics/icons/medium-biter.png"},
+        {icon = "__alien_power__/graphics/icons/tired.png"},
+    },
+	icon_size = 64,
+    flags = {},
+    subgroup = "alien-power",
+    order = "x",
+    stack_size = 1
+	},
+		{
+    type = "item",
+    name = "big-biter-power",
+    icons =
+	{
+        {icon = "__alien_power__/graphics/icons/big-biter.png"}
+    },
+    icon_size = 64,
+    flags = {},
+    subgroup = "alien-power",
+    order = "a",
+    stack_size = 300,
+    fuel_category = 'alien',
+    fuel_value = '250MJ',
+    burnt_result = 'big-used-biter'
+	},
+	{
+    type = "item",
+    name = "big-used-biter",
+    icons =
+	{
+        {icon = "__alien_power__/graphics/icons/big-biter.png"},
+        {icon = "__alien_power__/graphics/icons/tired.png"},
+    },
+	icon_size = 64,
+    flags = {},
+    subgroup = "alien-power",
+    order = "x",
+    stack_size = 1
+	},
+		{
+    type = "item",
+    name = "behemoth-biter-power",
+    icons =
+	{
+        {icon = "__alien_power__/graphics/icons/behemoth-biter.png"}
+    },
+    icon_size = 64,
+    flags = {},
+    subgroup = "alien-power",
+    order = "a",
+    stack_size = 300,
+    fuel_category = 'alien',
+    fuel_value = '250MJ',
+    burnt_result = 'behemoth-used-biter'
+	},
+	{
+    type = "item",
+    name = "behemoth-used-biter",
+    icons =
+	{
+        {icon = "__alien_power__/graphics/icons/behemoth-biter.png"},
         {icon = "__alien_power__/graphics/icons/tired.png"},
     },
 	icon_size = 64,
@@ -33,7 +123,7 @@ data:extend(
 	{
     type = "item",
     name = "small-biter-meat",
-    icon = "__alien_power__/graphics/icons/meat.png",
+    icon = "__alien_power__/graphics/icons/small-meat.png",
     icon_size = 32,
     icon_mipmaps = 6,
     flags = {},
