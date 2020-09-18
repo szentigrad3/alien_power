eec1ef46d7740f59d1a314e485d32d3a23cccb34
Pezzawinkle <54166225+Pezzawinkle@users.noreply.github.com>
2020-09-18 12:21:38 +1000

    added drop to all non-specified enemies

--- data-updates.lua
+++ data-updates.lua
@@ -1,1 +1,2 @@
 require("prototypes.dynamic-biters")
+AddLootToGenericEnemies()
--- data.lua
+++ data.lua
@@ -136,6 +136,20 @@ function AddLootToRampantEnemies()
 	end
 end
 
+function AddLootToGenericEnemies()
+  for i,k in pairs(data.raw.unit) do
+    if k.subgroup == "enemies" then
+      if k.loot then
+        if not k.loot["biter-meat"] then
+          table.insert(k.loot, { item = "biter-meat", probability = 0.5, count_min = 1, count_max = math.floor(5 + 0.5) })
+        end
+      else
+        k.loot = {{ item = "biter-meat", probability = 0.5, count_min = 1, count_max = math.floor(5 + 0.5) }}
+      end
+    end
+  end
+end
+
 AddLootToVanillaEnemies()
 AddLootToNEEnemies()
 AddLootToRampantEnemies()
