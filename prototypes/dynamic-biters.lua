2da98b7dc5f0ce729412f40c8b8c2258abe93891
szentigrad3 <nofukn@gmail.com>
2020-09-29 23:28:30 +1000

    make the recharing dynamic per tier

--- prototypes/dynamic-biters.lua
+++ prototypes/dynamic-biters.lua
@@ -6,31 +6,31 @@ for i,k in pairs(data.raw.unit) do
   if k.subgroup == "enemies" and (string.find(i,"biter") or string.find(i,"scarab")) then
     --[[local icons ="__base__/graphics/icons/small-biter.png"
     if k.icon then
-      icons=k.icon
-    elseif k.icons then
-      icons=k.icons
-    end]]
-    biter_listing[#biter_listing+1] = {
-      name = k.name,
-      health = k.max_health,
-      icon = k.icon or k.icons,
-      icon_size = k.icon_size or 32,
-      scale = k.run_animation.layers[2].scale,
-      tint_1 = k.run_animation.layers[2].tint, 
-      tint_2 = k.run_animation.layers[3].tint or nil
-    }
-    local add = true
-    for _,j in pairs(healths) do
-      if j[1]==k.max_health then
-        add = false
-        break
-      end
-    end
-    if add == true then
-      healths[#healths+1]={k.max_health}
-    end
-    --biter_define[#biter_define+1]=k
+    icons=k.icon
+  elseif k.icons then
+  icons=k.icons
+end]]
+biter_listing[#biter_listing+1] = {
+  name = k.name,
+  health = k.max_health,
+  icon = k.icon or k.icons,
+  icon_size = k.icon_size or 32,
+  scale = k.run_animation.layers[2].scale,
+  tint_1 = k.run_animation.layers[2].tint,
+  tint_2 = k.run_animation.layers[3].tint or nil
+}
+local add = true
+for _,j in pairs(healths) do
+  if j[1]==k.max_health then
+    add = false
+    break
   end
+end
+if add == true then
+  healths[#healths+1]={k.max_health}
+end
+--biter_define[#biter_define+1]=k
+end
 end
 --collect and sort the healths into an array to be sorted
 function compare(a,b)
@@ -70,66 +70,79 @@ for _,biter in pairs(biter_listing) do
       {icon = biter.icon, icon_size = biter.icon_size, tint=biter.tint_1}
     }
   end
+  local energy = 120
+  local amount = 55
+  if biter.fuel_category=="alien-1" then
+    energy = 30
+	amount = 5
+  elseif biter.fuel_category=="alien-2" then
+    energy = 60
+	amount = 15
+  elseif biter.fuel_category=="alien-3" then
+    energy = 90
+	amount = 45
+  end
   icsu=table.deepcopy(ics)
   icsu[#icsu]={icon = "__alien_power__/graphics/icons/tired.png",icon_size = 64}
   data:extend(
+  {
     {
-      {
-        type = "item",
-        name = biter.name .."-power",
-        localised_name = {"item-name.powered-biter",{"entity-name."..biter.name}},
-        icons = ics,
-        icon_size = biter.icon_size or 32,
-        flags = {},
-        subgroup = "alien-power",
-        order = "a["..biter.name.."]",
-        stack_size = 300,
-        fuel_category = biter.fuel_category,
-        fuel_value = biter.health .. "kJ",
-        burnt_result = biter.name .."-used"
-      },
-      {
-        type = "item",
-        name = biter.name .."-used",
-        localised_name = {"item-name.tired-biter",{"entity-name."..biter.name}},
-        icons = icsu,
-        icon_size = biter.icon_size or 32,
-        flags = {},
-        subgroup = "alien-power",
-        order = "x["..biter.name.."]",
-        stack_size = 1
-      },
-      {
+      type = "item",
+      name = biter.name .."-power",
+      localised_name = {"item-name.powered-biter",{"entity-name."..biter.name}},
+      icons = ics,
+      icon_size = biter.icon_size or 32,
+      flags = {},
+      subgroup = "alien-power",
+      order = "a["..biter.name.."]",
+      stack_size = 300,
+      fuel_category = biter.fuel_category,
+      fuel_value = biter.health .. "kJ",
+      burnt_result = biter.name .."-used"
+    },
+    {
+      type = "item",
+      name = biter.name .."-used",
+      localised_name = {"item-name.tired-biter",{"entity-name."..biter.name}},
+      icons = icsu,
+      icon_size = biter.icon_size or 32,
+      flags = {},
+      subgroup = "alien-power",
+      order = "x["..biter.name.."]",
+      stack_size = 1
+    },
+    {
       type = 'recipe',
       name = biter.name .."-recharging",
+      energy_required = energy,
       localised_name = {"recipe-name.recharging",{"entity-name."..biter.name}},
       category = 'alien-recharge',
+	  energy_required = energy,
       enabled = true,
       hide_from_player_crafting = true,
-      energy_required = 30, --possibly think about setting this per tier?
       ingredients = {
         {type = 'item', name = biter.name .."-used", amount = 1},
-        {type = 'item', name = 'biter-meat', amount = 5}, --possibly think about setting this per tier?
+        {type = 'item', name = 'biter-meat', amount = amount}, --possibly think about setting this per tier?
       },
       results = {
         {type = 'item', name = biter.name .."-power", amount = 1},
       },
     },
-	{
-    type = "recipe",
-    name = biter.name .."-breeding",
-	localised_name = {"recipe-name.breeding",{"entity-name."..biter.name}},
-	category = "alien-breeder",
-	energy_required = 300,
-    enabled = false,
-    ingredients =
     {
-      {"biter-egg", 1},
-    },
+      type = "recipe",
+      name = biter.name .."-breeding",
+      localised_name = {"recipe-name.breeding",{"entity-name."..biter.name}},
+      category = "alien-breeder",
+      energy_required = 300,
+      enabled = false,
+      ingredients =
+      {
+        {"biter-egg", 1},
+      },
       results = {
-        {type = 'item', name = biter.name .."-power", amount = 1},
+        {type = 'item', name = biter.name .."-power", probability=0.5, amount = 1},
+      },
     },
-  },
-	table.insert(data.raw["technology"]["capture-capsule-d"].effects, {type="unlock-recipe", recipe=biter.name .."-breeding"})
+    table.insert(data.raw["technology"]["capture-capsule-d"].effects, {type="unlock-recipe", recipe=biter.name .."-breeding"})
   })
 end
